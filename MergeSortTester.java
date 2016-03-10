4587edf9764ab228d99af89fa0c2b32eeaca54e0
Shaumik <a.shaumik@gmail.com>
2016-03-10 04:36:54 +0000

    done

--- MergeSortTester.java
+++ MergeSortTester.java
@@ -2,21 +2,33 @@
   class MergeSortTester
 
   ALGORITHM:
-  <INSERT YOUR DISTILLATION OF ALGO HERE>
+  	given parameter array
+	if array is sorted return array
+	else
+		split array into two halves, a and b
+		return the merge the recursion of a and the recursion of b
+	
+	merge: view first element of two arrays, and add the lowest element to the to-be-returned array
 
   BIG-OH CLASSIFICATION OF ALGORITHM:
-  <INSERT YOUR EXECUTION TIME CATEGORIZATION OF MERGESORT HERE>
+  O(n)
 
   Mean execution times for dataset of size n:
   Batch size: <# of times each dataset size was run>
-  n=1       time: 
-  n=10      time: 
-  n=100     time: 
+  n=1       time: -
+  n=10      time: ~2500 nanoseconds
+  n=100     time: ~12000 nanoseconds
   ...
-  n=<huge>  time: 
+  n=100000  time:~17 million nanoseconds 
+
+  REFER TO RESULTS.txt FOR FULL DATA
+  CASE REPRESENTS LENGTH OF ARRAY
+  TIME REPRESENTS TIME ELAPSED BY mergesort() IN NANOSECONDS
 
   ANALYSIS:
-  <INSERT YOUR RESULTS ANALYSIS HERE>
+  Case 10 of Experiments 5 and 0 are outliers
+  The number of digits in the number of the case is proportionate to the number of digits in time
+  log_base_10(CASE) + 2 = floor( log_base_10(TIME) )
   ======================================*/
 
 public class MergeSortTester 
@@ -29,7 +41,7 @@ public class MergeSortTester
 		int arr[] = new int[n];
 		
 		for(int i=0; i<n; i++) {
-			arr[i] = Math.random();
+			arr[i] = (int)(Math.random()*100);
 		}
 		
 		return( arr );
@@ -45,7 +57,7 @@ public class MergeSortTester
 	//startTime - a relative time marking start of count
 	//return - nanoseconds since startTime as a long
 	public static long getTimeSince(long startTime) {
-		return( startTime - System.nanoTime() );
+		return( Math.abs( startTime - System.nanoTime() ) );
 	}
 	
 	//time and run test of an n-element test case array and output results
@@ -56,12 +68,12 @@ public class MergeSortTester
 		
 		System.out.println("-----------------------------------------");
 		System.out.print("Sorting Array: ");
-		MergeSort.printArray(arr);
+		//MergeSort.printArray(arr);
 		t = getTime();
 		MergeSort.sort(arr);
 		t = getTimeSince(t);
 		
-		MergeSort.printArray(arr);
+		//MergeSort.printArray(arr);
 		System.out.println("Case: " + n + "| Time: " + t);
 		System.out.println();
 		System.out.println("-----------------------------------------");
@@ -76,16 +88,22 @@ public class MergeSortTester
      ******************************/
     public static void main( String[] args ) 
     {
-
-		MergeSortTester.runTest(10);
-		MergeSortTester.runTest(100);
-		MergeSortTester.runTest(200);
-		MergeSortTester.runTest(300);
-		MergeSortTester.runTest(1000);
-		MergeSortTester.runTest(2000);
-		MergeSortTester.runTest(3000);
-		MergeSortTester.runTest(10000);
-	
+		for(int i=0; i<10; i++) {
+			System.out.println("Experiment " + i + " ===================================");
+			MergeSortTester.runTest(10);
+			MergeSortTester.runTest(100);
+			//MergeSortTester.runTest(200);
+			//MergeSortTester.runTest(300);
+			System.gc();
+			MergeSortTester.runTest(1000);
+			//MergeSortTester.runTest(2000);
+			//MergeSortTester.runTest(3000);
+			System.gc();
+			MergeSortTester.runTest(10000);
+			System.gc();
+			MergeSortTester.runTest(100000);
+			System.gc();
+		}
     }//end main
 
 }//end class
