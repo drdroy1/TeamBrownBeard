2c3a4db64c45b22c5aa4b2428d80f50f2fbc32a0
Shaumik-Ashraf <a.shaumik@gmail.com>
2016-03-09 19:40:23 -0800

    updates

--- MergeSortTester.java
+++ MergeSortTester.java
@@ -22,6 +22,52 @@
 public class MergeSortTester 
 {
 
+	//create a new int array of size n and fill it with random values
+	//n - positive int, length of array
+	//return - newly allocated array of random elements,
+	public static int[] generate_array(int n) {
+		int arr[] = new int[n];
+		
+		for(int i=0; i<n; i++) {
+			arr[i] = math.random();
+		}
+		
+		return( arr );
+	}
+
+	//for timing methods
+	//return a relative start time in nanoseconds as a long
+	public static long getTime() {
+		return( System.nanoTime() );
+	}
+	
+	//for timing methods
+	//startTime - a relative time marking start of count
+	//return - nanoseconds since startTime as a long
+	public static long getTimeSince(long startTime) {
+		return( startTime - System.nanoTime() );
+	}
+	
+	//time and run test of an n-element test case array and output results
+	public static void runTest( int n ) {
+		
+		long t;
+		int arr[] = generate_array(n);
+		
+		System.out.println("-----------------------------------------");
+		System.out.print("Sorting Array: ");
+		MergeSort.printArray(arr);
+		t = getTime();
+		MergeSort.sort(arr);
+		t = getTimeSince(t);
+		
+		MergeSort.printArray(arr);
+		System.out.println("Case: " + n + "| Time: " + t);
+		System.out.println();
+		System.out.println("-----------------------------------------");
+		
+	}
+	
     /******************************
      * execution time analysis 
      * <INSERT YOUR DESCRIPTION HERE OF 
@@ -31,6 +77,15 @@ public class MergeSortTester
     public static void main( String[] args ) 
     {
 
+		MergeSortTester.runTest(10);
+		MergeSortTester.runTest(100);
+		MergeSortTester.runTest(200);
+		MergeSortTester.runTest(300);
+		MergeSortTester.runTest(1000);
+		MergeSortTester.runTest(2000);
+		MergeSortTester.runTest(3000);
+		MergeSortTester.runTest(10000);
+	
     }//end main
 
 }//end class
